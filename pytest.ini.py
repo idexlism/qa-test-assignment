716537ba1bea469cae4265bdd1adf795c243ec3a
Elbeg Budaev <dlyaorigina2002@mail.ru>
2026-04-04 01:55:19 +0200

    configured file

---
+++ pytest.ini.py
@@ -0,0 +1,5 @@
+[pytest]
+# URL сайта
+base_url = https://cerulean-praline-8e5aa6.netlify.app/
+# тесты без окна браузера
+addopts = --headed --browser chromium --base-url=https://cerulean-praline-8e5aa6.netlify.app/
